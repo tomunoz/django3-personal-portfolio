cf5991947e1ef60c3f11cda79e49c6c169c6fad8
Tom Munoz <tom.o.munoz@gmail.com>
2022-02-01 10:26:19 -0500

    Modified settings.py for pythonanywhere deploy, created local_setting.py and .gitignore.

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q3ct@g-9nvua46pak^p9%q-b6f!4z!))j*cnid-ccf7)6_jpte'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tommunoz.pythonanywhere.com']
 
 
 # Application definition
@@ -121,7 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on productoin.")
