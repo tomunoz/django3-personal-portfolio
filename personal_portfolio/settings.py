0a11f8e2a323b50ef6ba29ff7cdc97f372e1379d
Tom Munoz <tom.o.munoz@gmail.com>
2022-02-16 17:06:34 -0500

    Remove more whitenoise things to get the app to start up again.

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    """ 'whitenoise.middleware.WhiteNoiseMiddleware', """
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,7 +122,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-""" STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' """
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
